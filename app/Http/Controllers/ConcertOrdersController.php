4c6f31f221dfb96c13e1be3937a79e24bf76df6d
Enner Escobedo <eriec.42@gmail.com>
2018-02-15 15:48:21 -0600

    Finish section Faking the payment gateway

---
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Billing\PaymentGateway;
+use App\Concert;
+
+class ConcertOrdersController extends Controller
+{
+
+    private $paymentGateway;
+
+    /**
+     * ConcertOrdersController constructor.
+     *
+     * @param $paymentGateway
+     */
+    public function __construct(PaymentGateway $paymentGateway)
+    {
+        $this->paymentGateway = $paymentGateway;
+    }
+
+
+    public function store($concertId)
+    {
+        $concert = Concert::find($concertId);
+        $ticketQuantity = request('ticket_quantity');
+        $amount = $ticketQuantity * $concert->ticket_price;
+        $token = request('payment_token');
+        $this->paymentGateway->charge($amount, $token);
+        return response()->json([], 201);
+    }
+}
